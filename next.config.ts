956b89c67497734fa77b0cdceb59f26030f2d6c5
Zanga Ouattara <ouattarazanga083@gmail.com>
2025-12-11 13:26:00 +0000

    l'image de la voiture ne s'afficher

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'brphhleywydbocpbtgtw.supabase.co',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
